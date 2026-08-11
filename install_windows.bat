ab2cae70af41dfe11de1751e349d23fce4a2f303
tstelnick2-cyber <tstelnick2@gmail.com>
2026-08-11 16:06:22 -0700

    Fix Windows install shortcut PowerShell syntax

--- install_windows.bat
+++ install_windows.bat
@@ -17,17 +17,7 @@ set "SHORTCUT_NAME=BarcodeMakerPro.lnk"
 set "DESKTOP_SHORTCUT=%USERPROFILE%\Desktop\%SHORTCUT_NAME%"
 set "START_MENU_SHORTCUT=%APPDATA%\Microsoft\Windows\Start Menu\Programs\%SHORTCUT_NAME%"
 
-powershell -NoProfile -Command "${
-  $w = New-Object -ComObject WScript.Shell;
-  foreach ($path in @('%DESKTOP_SHORTCUT%', '%START_MENU_SHORTCUT%')) {
-    $shortcut = $w.CreateShortcut($path);
-    $shortcut.TargetPath = 'python';
-    $shortcut.Arguments = '"%SCRIPT%" --gui';
-    $shortcut.WorkingDirectory = '%WORKDIR%';
-    $shortcut.IconLocation = '$env:SystemRoot\\System32\\shell32.dll,1';
-    $shortcut.Save();
-  }
-}"
+powershell -NoProfile -Command "& { $w = New-Object -ComObject WScript.Shell; foreach ($path in @('%DESKTOP_SHORTCUT%', '%START_MENU_SHORTCUT%')) { $shortcut = $w.CreateShortcut($path); $shortcut.TargetPath = 'python'; $shortcut.Arguments = '"%SCRIPT%" --gui'; $shortcut.WorkingDirectory = '%WORKDIR%'; $shortcut.IconLocation = '$env:SystemRoot\\System32\\shell32.dll,1'; $shortcut.Save(); } }"
 
 if %ERRORLEVEL% neq 0 (
     echo Failed to create shortcuts. You can still run the app with run_windows.bat.
